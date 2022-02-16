dc1b01482dd80e6203557a4f955da56b5a4108f9
eriksca <73497621+eriksca@users.noreply.github.com>
2022-02-16 14:59:31 +0100

    Update Shader Dissolve

--- VR_FITSTI_Ricordi/Assets/Ricordi_VRProject/Erik/Scripts/FadeManager.cs
+++ VR_FITSTI_Ricordi/Assets/Ricordi_VRProject/Erik/Scripts/FadeManager.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class FadeManager : MonoBehaviour
 {
-    [SerializeField] Material prova;
-    const string fadePos = "FadePos";
-    [SerializeField] float fadeHeight=0;
+    
+    const string cutoffHeight = "_CutoffHeight";
+   
     [Range(0.0f,0.017f)]
     [SerializeField] float incrementHeight;
     [Range(0.0f, 0.04f)]
@@ -15,32 +15,48 @@ public class FadeManager : MonoBehaviour
   
     void Start()
     {
-        Debug.Log(prova.GetFloat(fadePos));
+       
         foreach(Material mat in matArray)
         {
-            mat.SetFloat(fadePos, 0);
+            mat.SetFloat(cutoffHeight, -0.5f);
+            if (mat != matArray[0])
+            {
+                TestFade(mat, Random.Range(1, 15f));
+            }
+            else
+            {
+                TestFade(mat, 0.5f);
+            }
+           
         }
 
-        TestFade(matArray[0]);
-        TestFade(matArray[1]);
+       // TestFade(matArray[0], 1);
+        //TestFade(matArray[1], 6);
+        //TestFade(matArray[1]);
 
     }
 
-     void TestFade(Material matToFade)
+
+
+    void TestFade(Material matToFade,float delay)
     {
-        
+       
         Coroutine fadeCoroutine = null;
         if(fadeCoroutine == null)
         {
 
             IEnumerator _testfade()
             {
+                yield return new WaitForSeconds(delay);
+
+                Debug.Log($"Start fade on {matToFade.name}");
+                float fadeHeight = 0;
                 int testcont = 0;
                 while (fadeHeight < 10)
                 {
-                    Debug.Log(fadeHeight);
+                    //Debug.Log(fadeHeight);
                     fadeHeight += incrementHeight;
-                    matToFade.SetFloat(fadePos, fadeHeight);
+                    matToFade.SetFloat(cutoffHeight, fadeHeight);
                     yield return new WaitForSeconds(timeBetweenUpdates);
                     testcont++;
 
