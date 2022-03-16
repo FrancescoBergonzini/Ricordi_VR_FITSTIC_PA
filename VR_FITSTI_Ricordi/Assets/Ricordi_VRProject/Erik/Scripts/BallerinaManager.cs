6861e72552e8608e8870613adc0b7dbe489fb54c
eriksca <73497621+eriksca@users.noreply.github.com>
2022-03-16 13:02:24 +0100

    Ballerina Manager

--- VR_FITSTI_Ricordi/Assets/Ricordi_VRProject/Erik/Scripts/BallerinaManager.cs
+++ VR_FITSTI_Ricordi/Assets/Ricordi_VRProject/Erik/Scripts/BallerinaManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
@@ -12,28 +10,65 @@ namespace BNG
         [SerializeField] Transform midAirPoint;
         [SerializeField] Transform[] pezziPos;
 
+        MeshRenderer _myMeshRender;
+        static int statoPezziPresi = 0;
+
+        private void Start()
+        {
+            _myMeshRender = GetComponent<MeshRenderer>();
+        }
         private void OnTriggerEnter(Collider other)
         {
-            // l'oggetto per un attimo cade, sistemare questa cosa
-            if (other.GetComponent<Grabbable>() != null)
+            Debug.LogWarning("1");
+            if (!other.TryGetComponent(out PezzoBambola pezzo))
+            {              
+                return;
+            }
+            else
             {
-                Grabbable obj = other.GetComponent<Grabbable>();
-                Rigidbody obj_rb= other.gameObject.GetComponent<Rigidbody>();
-                if (obj.BeingHeld)
+                if (other.gameObject.tag == "Bambola" && statoPezziPresi == (int)pezzo.myself)
                 {
-                    Debug.Log("Devi mollare obj");
-                    return;
-                }
-                else
-                {
+                    Debug.LogWarning("2");
+
+                    Rigidbody obj_rb = other.gameObject.GetComponent<Rigidbody>();
+
                     Debug.Log("Parte il tween");
                     obj_rb.isKinematic = true;
-                    pezziPos[0].transform.GetChild(0).transform.DOMove(midAirPoint.transform.position, 4f).OnComplete(() =>
-                    pezziPos[0].transform.GetChild(0).transform.DOLocalMove(Vector3.zero, 4f));
+                    other.enabled = false;
+                    other.transform.DOMove(midAirPoint.transform.position, 4f).OnComplete(() =>
+                    other.transform.DOMove(pezziPos[(int)pezzo.myself].position, 4f));
+                    other.transform.DOScale(1, 2);
+
+                    //aggiorno la lo stato interno 
+                    statoPezziPresi++;
                 }
+            }
+                
+
+            // l'oggetto per un attimo cade, sistemare questa cosa
+           
+        }
+
+       /* private void Update()
+        {
+            if (PezzoBambola.oneOfAsIsGrabbed)
+            {
+                cilindermat.color = Color.yellow;
                 
             }
+            else
+            {
+                cilindermat.color = Color.blue;
+            }
+        }*/
+
+        public  void EnableMeshRendere(bool fallo)
+        {
+            _myMeshRender.enabled = fallo;
         }
 
     }
 }
+
+
+
